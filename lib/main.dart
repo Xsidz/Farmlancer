99c2916c6f34bb8431d0811ed30bb514587e46e8
Xsidz <siddhesh.kabraa@gmail.com>
2024-06-28 11:32:18 +0530

    8 pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
-import 'package:farmlancer/pages/login_page.dart';
-import 'package:farmlancer/pages/splash_screen.dart';
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
+import 'package:farmlancer/notifications/data_permission.dart';
+import 'package:farmlancer/notifications/loc_permission.dart';
+import 'package:farmlancer/notifications/not_permission.dart';
+import 'package:farmlancer/pages/cnf_pass.dart';
+import 'package:farmlancer/pages/home_menu.dart';
+import 'package:farmlancer/pages/signin_page.dart';
+import 'package:farmlancer/pages/signup_page.dart';
+import 'package:farmlancer/screens/spalsh_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,14 +17,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
       routes: {
-        '/splashscreen': (context) => SplashScreen(),
-        '/signuppage': (context) => LoginPage(), //yess
+        '/main' : (context) => MyApp() ,
+        '/location' : (context) => LocPermission(),
+        '/notification' : (context) => NotPermission(),
+        '/data' : (context) => Data(),
+        '/signup' : (context) => SignupPage(),
+        '/signin' : (context) => SigninPage(),
+        '/cnfpass'  : (context) => ConfPass(),
+        '/homepage' : (context)=>HomePage(),
       },
     );
   }
