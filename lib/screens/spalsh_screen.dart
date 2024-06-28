3e4431661860126b747990f2285a0095424d7d8f
Xsidz <siddhesh.kabraa@gmail.com>
2024-06-28 22:59:36 +0530

    minor changes

--- lib/screens/spalsh_screen.dart
+++ lib/screens/spalsh_screen.dart
@@ -4,9 +4,7 @@ import 'package:farmlancer/notifications/loc_permission.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-// void main() {
-//   runApp(MyApp());
-// }
+
 
 class MyApp extends StatelessWidget {
   @override
